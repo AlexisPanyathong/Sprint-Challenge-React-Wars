937d91003c67295c4b7ba7db673ecffbcdb8b42d
Alexis Panyathong <alexispanyathong@gmail.com>
2019-07-19 12:28:31 -0500

    I am getting confused...
    I got something to show up as an image tag on my web browser...

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import ImageCard from "./components/ImageCard";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,22 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people/')
+    .then(response => {
+      console.log(`people api`, response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <div>
+        <ImageCard key={people.results} imgUrl={people.results}/>
+      </div>
+      
     </div>
   );
 }
